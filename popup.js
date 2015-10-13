f6b3e70693a0cf1ee5543792d970bb76c3d845d4
Thomas Babu <thomas@interuptive.com>
2015-10-13 18:55:31 +0800

    base files

--- popup.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function(){
-	var bookmarkMeButton = document.getElementById("bookmarkMe");
-	bookmarkMeButton.addEventListener('click', function(){
-		chrome.tabs.getSelected(null, function(tab) {
-			alert(tab.url);
-			return false;
-		});
-	}, false);
-}, false);
